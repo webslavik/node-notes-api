20ccc9ea0e55621d67bd45ba88db636c47ee9121
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2018-02-05 22:28:56 +0200

    update Mongo url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-const local_db_url = 'mongodb://localhost:27017/TodoApp';
-const mlab_url = 'mongodb://<dbuser>:<dbpassword>@ds125318.mlab.com:25318/node-todo-api';
+const REMOTE_MONGO = 'mongodb://<dbuser>:<dbpassword>@ds125318.mlab.com:25318/node-todo-api';
+const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mlab_url);
+mongoose
+  .connect(MONGO_URI)
+  .then(
+    () => console.log('Connected to Mongo instance.'),
+    error => console.log('Error connecting to Mongo instance: ', error)
+  );
 
 module.exports = {
   mongoose
