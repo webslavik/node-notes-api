aa5fc6c03ad209d9d942f5eef0a8dcf8b6b4169e
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2018-02-05 23:03:19 +0200

    fix connection to mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const REMOTE_MONGO = 'mongodb://<dbuser>:<dbpassword>@ds125318.mlab.com:25318/node-todo-api';
+const REMOTE_MONGO = 'mongodb://test-user:test-user@ds125318.mlab.com:25318/node-todo-api';
 const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
 const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
 
 mongoose.Promise = global.Promise;
-mongoose
-  .connect(MONGO_URI)
+mongoose.connect(MONGO_URI)
   .then(
     () => console.log('Connected to Mongo instance.'),
     error => console.log('Error connecting to Mongo instance: ', error)
