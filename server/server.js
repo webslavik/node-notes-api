351cb3d1e4ce55596f9aa6ab8a490f0000c30ac8
Slavik <slavik.sukhanov@iskyweb.com>
2018-02-05 12:07:38 +0200

    add Mongoose validators, types, defaults

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -27,9 +32,7 @@ const Todo = mongoose.model('Todo', {
 //   );
 
 const otherTodo = new Todo({
-  text: 'Some awesome todo',
-  completed: false,
-  completedAt: 123
+  text: 'Wash the dog',
 });
 
 otherTodo
@@ -38,3 +41,29 @@ otherTodo
     result => console.log(JSON.stringify(result, undefined, 2)),
     error => console.log('Unable to save todo.')
   );
+
+
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String
+//   },
+//   email: {
+//     type: String,
+//     require: true,
+//     minlength: 1,
+//     trim: true,
+//   }
+// });
+
+// const newUser = new User({
+//   name: 'Bob',
+//   email: 'bob@gmail.com'
+// });
+
+// newUser
+//   .save()
+//   .then(
+//     result => console.log(result),
+//     error => console.log(error)
+//   )
+
