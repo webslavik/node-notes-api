9e204e58b8aba2ae0eb12a7cc74d219d4836b96b
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2018-02-05 22:00:11 +0200

    update Port and package.json; add mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
+const local_db_url = 'mongodb://localhost:27017/TodoApp';
+const mlab_url = 'mongodb://<dbuser>:<dbpassword>@ds125318.mlab.com:25318/node-todo-api';
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(mlab_url);
 
 module.exports = {
   mongoose
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -53,7 +54,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log(`Started up on port ${port}`);
 });
 
 module.exports = {app};
