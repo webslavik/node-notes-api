c6650f264e41a7329216ac8dcefd352bcd0b16da
Slavik <slavik.sukhanov@iskyweb.com>
2018-02-06 17:04:51 +0200

    add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const {ObjectID} = require('mongodb');
 const bodyParser = require('body-parser');
@@ -53,6 +54,7 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => res.status(400).send());
 });
 
+// delete Todo item
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id;
 
@@ -68,6 +70,32 @@ app.delete('/todos/:id', (req, res) => {
       res.status(200).send({todo});
     })
     .catch(error => res.status(400).send());
+});
+
+// update Todo item
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({errorMessage: 'Not valid ID'});
+  }
+
+   if (_.isBoolean(body.completed) && body.completed) {
+     body.completedAt = new Date().getTime();
+   } else {
+     body.completed = false;
+     body.completedAt = null;
+   }
+
+   Todo
+    .findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then((todo) => {
+      if (!todo) return res.status(404).send({errorMessage: 'Not found item'});
+
+      res.status(200).send({todo});
+    })
+    .catch(error => res.status(400).send(error));
 });
 
 app.listen(port, () => {
